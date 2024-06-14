be29ba735eae2799b72b7d4cc6a75291a35faf6c
Mahindra Guptha Kotha <mahindragk22@gmail.com>
2024-06-15 03:49:14 +0530

    Update st_app.py

--- sheets_Data_updation/st_app.py
+++ sheets_Data_updation/st_app.py
@@ -7,5 +7,4 @@ url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
+conn.update(spreadsheet=url, "reponses")
