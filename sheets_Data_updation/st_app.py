cf894f9b85e25249db786d5eb174945ca20a8edc
Mahindra Guptha Kotha <mahindragk22@gmail.com>
2024-06-15 12:11:04 +0530

    Update st_app.py

--- sheets_Data_updation/st_app.py
+++ sheets_Data_updation/st_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+import gspread
 
+# Google Sheets URL
 url = 'https://docs.google.com/spreadsheets/d/178sSyO5YpLNOVz8XtZJTif6Vc07-K7Nncjp56TB3qb8/edit?usp=sharing'
 
+# Initialize GSheetsConnection
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-
-# Read data from the Google Sheet
 df = conn.read(spreadsheet=url, usecols=[0, 1])
 st.dataframe(df)
 
@@ -21,7 +22,11 @@ if st.button("Submit"):
         df = pd.concat([df, new_row], ignore_index=True)
         
         # Update the Google Sheet with the new data
-        conn.update(spreadsheet=url, dataframe=df)
+        gc = gspread.service_account()  # Adjust if using service account credentials
+        sh = gc.open_by_url(url)
+        worksheet = sh.sheet1
+        worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+        
         st.success("Data added successfully!")
     else:
         st.error("Please enter some data.")
