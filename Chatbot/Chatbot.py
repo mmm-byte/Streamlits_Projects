34fa7b8a1fa3e3bc572dacb97960a59d1ba7d783
Mahindra Guptha Kotha <mahindragk22@gmail.com>
2024-06-22 01:02:59 +0530

    Update Chatbot.py

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -2,19 +2,24 @@ import streamlit as st
 import google.generativeai as genai
 import streamlit as st
 import gspread
+import toml
 from gspread_dataframe import set_with_dataframe
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 from googletrans import Translator
+
+# Load the TOML file
+config = toml.load('config.toml')
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-genai.configure(api_key="AIzaSyAQaD147XtW0Yr9ZQlgRhe6bBRsIHsEAeE")
+genai.configure(api_key="config['api']['key']")
 # for m in genai.list_models():
 #   if 'generateContent' in m.supported_generation_methods:
 #     print(m.name)
